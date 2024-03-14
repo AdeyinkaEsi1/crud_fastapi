d84c4230e4625045b0df3b924253ba7635379cc4
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-03-14 21:49:45 +0100

    Add UserSchema.py with UserBase and UserCreate classes

---
+++ schemas/UserSchema.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+from pydantic import EmailStr
+
+
+class UserBase(BaseModel):
+   email: EmailStr
+class UserCreate(UserBase):
+   lname: str
+   fname: str
+   password: str
