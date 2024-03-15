9159a03cc262dfff0d5b69cce5ee963670ac96d3
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-03-15 21:23:14 +0100

    imp:formatting

--- settings.py
+++ settings.py
@@ -18,12 +18,12 @@ def get_db():
 def get_current_user(db: Session = Depends(get_db),
                 	token: str = Depends(oauth2_scheme)):
    return decode_access_token(db, token)
+
+
 @app.get("/api/me", response_model=UserSchema)
 def read_logged_in_user(current_user: User = Depends(get_current_user)):
    """return user settings for current user"""
    return current_user
-
-
 
 
 def create_todo(db: Session, current_user: User, todo_data: TodoSchema.TODOCreate):
@@ -35,6 +35,7 @@ def create_todo(db: Session, current_user: User, todo_data: TodoSchema.TODOCreat
    db.refresh(todo)
    return todo
 
+
 def update_todo(db: Session, todo_data: TodoSchema.TODOUpdate):
    todo = db.query(TODO).filter(TODO.id == id).first()
    todo.text = todo_data.text
@@ -43,10 +44,12 @@ def update_todo(db: Session, todo_data: TodoSchema.TODOUpdate):
    db.refresh(todo)
    return todo
 
+
 def delete_todo(db: Session, id: int):
    todo = db.query(TODO).filter(TODO.id == id).first()
    db.delete(todo)
    db.commit()
+ 
  
 def get_user_todos(db: Session, userid: int):
    return db.query(TODO).filter(TODO.owner_id == userid).all()
