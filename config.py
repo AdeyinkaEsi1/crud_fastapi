974e4cc2135aec74e75aef8a9372446bbb1780fd
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-03-16 21:21:10 +0100

    upd

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.environ("SQLALCHEMY_DATABASE_URL")
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-Sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base
+
+
+
+
+# import os
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+
+# SQLALCHEMY_DATABASE_URL = os.environ("SQLALCHEMY_DATABASE_URL")
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base
