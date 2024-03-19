aa614883e432638e164825489e48bab572681d1e
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-03-19 16:50:09 +0100

    upd

--- models/model.py
+++ models/model.py
@@ -1,30 +1,37 @@
 from enum import Enum
-from typing import List, Optional
-from uuid import UUID, uuid4
 from pydantic import BaseModel
+from uuid import uuid4, UUID
+from typing import List, Optional
 
 
 class Role(str, Enum):
-    admin = "asmin"
+    admin = "admin"
     user = "user"
-
 
 class Gender(str, Enum):
     male = "male"
     female = "female"
 
 class User(BaseModel):
-    id: Optional[UUID] = uuid4()
+    id: Optional[UUID] = uuid4
     first_name: str
     last_name: str
     gender: Gender
     roles: List[Role]
 
-
 class UpdateUser(BaseModel):
     first_name: Optional[str]
     last_name: Optional[str]
     roles: Optional[List[Role]]
+
+
+
+
+from enum import Enum
+from typing import List, Optional
+from uuid import UUID, uuid4
+from pydantic import BaseModel
+
 
 
 
