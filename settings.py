653e17499526d86642daa9f1baf1835aeb38b3b7
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-03-25 22:49:10 +0100

    added mongo list_db

---
+++ settings.py
@@ -0,0 +1,25 @@
+#mongo_uri = mongodb+srv://Hamid:Hmongodb1@cluster0.zaaveoa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+"""
+connection str = mongodb+srv://Hamid:<password>@cluster0.zaaveoa.mongodb.net/
+"""
+# 102.88.33.58
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+MONGODB_URI = "mongodb+srv://Hamid:Hmongodb1@cluster0.zaaveoa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(MONGODB_URI, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+for db in client.list_database_names():
+    print(db)
+
