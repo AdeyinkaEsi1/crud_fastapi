25c8eb2288bc91723b620f7912f8b45c39da4ec0
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-04-05 20:34:25 +0100

    formatted

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ from fastapi import FastAPI, HTTPException
 from models.model import Gender, Role, User
 from models.model import UpdateUser
 
-
 app = FastAPI()
-
 
 db: List[User] = [User(
  id=uuid4(),
@@ -15,10 +13,7 @@ db: List[User] = [User(
  gender=Gender.male,
  roles=[Role.user],
  ),
-# ===================================================================================== short test summary info ====================================================================================== 
-# ERROR tests/routes/test_presets.py::TestPresets::test_create_and_delete_preset_integration - pymongo.errors.ServerSelectionTimeoutError: 
-# SSL handshake failed: ac-ukcsyby-shard-00-00.k7xys16.mongodb.net:27017: _ssl.c:989: 
-# The handshake operation timed out,ac-ukcsyby-shard-00-02.k7xys16...
+
  User(
  id=uuid4(),
  first_name="Jane",
